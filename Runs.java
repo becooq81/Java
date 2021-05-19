8aaada3f27b25d552e633ed98daa4cc2f3e61a37
jinny-codes <77732679+jinny-codes@users.noreply.github.com>
2021-05-19 15:35:13 +0900

    Add files via upload

---
+++ Runs.java
@@ -0,0 +1,32 @@
+//finds the length of the longest run of heads in 100 flips of a coin
+
+public class Runs 
+{
+    public static void main(String[] args) {
+        final int FLIPS = 100; // number of coin flips
+        
+        int currentRun = 0; // length of the current run of HEADS
+        int maxRun = 0; // length of the maximum run so far 
+        
+        // Create a coin object
+        Coin coin1 = new Coin();
+        //Flip the coin FLIPS times
+        for (int i = 0; i < FLIPS; i++)
+        {
+            //Flip the coin & print the result
+            coin1.flip();
+            if (coin1.getFace() == coin1.HEADS) {
+                currentRun++;
+            }
+            else {
+                if (currentRun>maxRun){
+                    maxRun=currentRun;
+                }
+                currentRun=0;
+            }
+            // Update the run information 
+        }
+        System.out.println("The maximum number of runs is " + maxRun);
+        
+    }
+}
