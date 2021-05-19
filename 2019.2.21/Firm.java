63de31209fa0515ebd3ea0eaf2b0dbb5f62366f9
jinny-codes <77732679+jinny-codes@users.noreply.github.com>
2021-05-19 16:20:42 +0900

    Add files via upload

---
+++ 2019.2.21/Firm.java
@@ -0,0 +1,8 @@
+public class Firm {
+    // creates a staff of employees for a firm and pays them
+    public static void main(String[] args) {
+        Staff personnel = new Staff();
+        personnel.payday();
+    }
+    
+}
